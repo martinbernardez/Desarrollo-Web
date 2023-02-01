006c707ea6112593c4a19413c7f4166b08667c67
Martín Bernardez <martynbernardez@gmail.com>
2023-02-01 07:28:17 -0300

    Añadimos js al html

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+/**VARIABLES: Identificadores únicos. Los nombres de las variables no pueden comenzar con números, 
+carácteres especiales ni palabras reservadas de JS. No es lo mismo mayúsculas y/o minúsculas.*/ 
+
+//COMENTARIO MULTILINEA 
+/**
+ * 
+ */
+
+//COMENTARIO DE UNA SOLA LINEA //
+
+
+//Variables (pueden ir cambiando durante el programa) 
+var miVariable;      //VAR se usó desde 1995 a 2015 (navegadores viejos)
+let nombre;          //LET se usa a partir de 2015 (actual)
+
+//Constantes (son variabels que no cambian durante el programa)
+const IVA;
+
+
